0a57603424da397758810e2baa924190d75d3a06
Chuseok22 <bjh59629@naver.com>
2026-01-03 01:34:54 +0900

    프로젝트_멀티모듈_구조_도입 : feat : [common 모듈] build.gradle.kts 설정 https://github.com/CampusTable/campus-table-server/issues/1

---
+++ CT-common/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+  id("java-library")
+}
+
+tasks.bootJar {
+  enabled = false
+}
+
+tasks.jar {
+  enabled = true
+  archiveClassifier.set("")
+}
+
+// common 모듈 api 의존성은 모든 모듈에 적용
+dependencies {
+  // Spring Starter Web
+  api(libs.spring.boot.starter.web)
+
+  // JPA
+  api(libs.spring.boot.starter.data.jpa)
+
+  // Validation
+  api(libs.spring.boot.starter.validation)
+
+  // Jackson
+  api(libs.jackson.module.kotlin)
+
+  api(libs.kotlin.reflect)
+  api(libs.kotlin.stdlib)
+}
+
