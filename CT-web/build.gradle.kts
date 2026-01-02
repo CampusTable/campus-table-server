61da421086fad8d74d401a7c77e09b5a675cf1bc
Chuseok22 <bjh59629@naver.com>
2026-01-03 01:35:41 +0900

    프로젝트_멀티모듈_구조_도입 : feat : [web 모듈] build.gradle.kts 설정 https://github.com/CampusTable/campus-table-server/issues/1

---
+++ CT-web/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+  id("java-library")
+}
+
+tasks.bootJar {
+  enabled = false
+}
+
+tasks.jar {
+  enabled = true
+  archiveClassifier.set("")
+}
+
+dependencies {
+  implementation(project(":CT-auth"))
+  implementation(project(":CT-common"))
+  implementation(project(":CT-member"))
+}
+
