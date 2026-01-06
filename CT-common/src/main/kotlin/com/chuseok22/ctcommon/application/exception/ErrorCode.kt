129c64ce54c2ef1947546c9855c31adf9baa1145
Chuseok22 <bjh59629@naver.com>
2026-01-06 10:54:56 +0900

    Spring_Security_기본_설정 : feat : CustomException 커스텀 예외처리 로직 추가 https://github.com/CampusTable/campus-table-server/issues/3

---
+++ CT-common/src/main/kotlin/com/chuseok22/ctcommon/application/exception/ErrorCode.kt
@@ -0,0 +1,17 @@
+package com.chuseok22.ctcommon.application.exception
+
+import org.springframework.http.HttpStatus
+
+enum class ErrorCode(
+  val status: HttpStatus,
+  val message: String
+) {
+
+  // Global
+  INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버에 문제가 발생했습니다"),
+  ACCESS_DENIED(HttpStatus.FORBIDDEN, "접근이 거부되었습니다."),
+  UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "인증이 필요합니다"),
+
+  // JWT
+  INVALID_JWT(HttpStatus.UNAUTHORIZED, "유효하지 않은 JWT"),
+}
