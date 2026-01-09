36e80a3ec2737337c830503317c34b351d5277f7
Chuseok22 <bjh59629@naver.com>
2026-01-09 16:42:13 +0900

    Spring_Security_기본_설정 : feat : JPA Auditing 활성화 https://github.com/CampusTable/campus-table-server/issues/3

--- CT-web/src/main/kotlin/com/chuseok22/ctweb/CampusTableServerApplication.kt
+++ CT-web/src/main/kotlin/com/chuseok22/ctweb/CampusTableServerApplication.kt
@@ -2,8 +2,10 @@ package com.chuseok22.ctweb
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
+@EnableJpaAuditing
 class CampusTableServerApplication
 
 fun main(args: Array<String>) {
