8fc4bc257c79a24b3b45ee20b060b1bc891f4fee
Chuseok22 <bjh59629@naver.com>
2026-01-09 13:28:27 +0900

    Spring_Security_기본_설정 : feat : JwtStore 추가 https://github.com/CampusTable/campus-table-server/issues/3

---
+++ CT-auth/src/main/kotlin/com/chuseok22/ctauth/infrastructure/jwt/JwtStore.kt
@@ -0,0 +1,17 @@
+package com.chuseok22.ctauth.infrastructure.jwt
+
+import com.chuseok22.ctauth.core.token.TokenStore
+import org.springframework.data.redis.core.RedisTemplate
+import java.util.concurrent.TimeUnit
+
+class JwtStore(
+  private val redisTemplate: RedisTemplate<String, String>
+) : TokenStore {
+  override fun save(key: String, refreshToken: String, ttlMillis: Long) {
+    redisTemplate.opsForValue().set(key, refreshToken, ttlMillis, TimeUnit.MILLISECONDS)
+  }
+
+  override fun remove(key: String) {
+    redisTemplate.delete(key)
+  }
+}
