53589da014396649cf88a8aa497f75369b209a9f
Chuseok22 <bjh59629@naver.com>
2026-01-11 20:43:50 +0900

    Spring_Security_기본_설정 : feat : JPA Auditing 을 위한 BaseEntity.kt 추상클래스 추가 https://github.com/CampusTable/campus-table-server/issues/3

---
+++ CT-common/src/main/kotlin/com/chuseok22/ctcommon/infrastructure/persistance/BaseEntity.kt
@@ -0,0 +1,48 @@
+package com.chuseok22.ctcommon.infrastructure.persistance
+
+import jakarta.persistence.Column
+import jakarta.persistence.EntityListeners
+import jakarta.persistence.MappedSuperclass
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.Instant
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener::class)
+abstract class BaseEntity {
+
+  /**
+   * 생성일시 (UTC)
+   */
+  @field:CreatedDate
+  @field:Column(name = "created_at", nullable = false, updatable = false, columnDefinition = "TIMESTAMPTZ")
+  var createdAt: Instant? = null
+
+  /**
+   * 수정일시 (UTC)
+   */
+  @field:LastModifiedDate
+  @field:Column(name = "updated_at", nullable = false, columnDefinition = "TIMESTAMPTZ")
+  var updatedAt: Instant? = null
+
+  /**
+   * 삭제 여부
+   */
+  @field:Column(name = "is_deleted", nullable = false)
+  var isDeleted: Boolean = false
+
+  /**
+   * 삭제일시 (UTC)
+   */
+  @field:Column(name = "deleted_at", columnDefinition = "TIMESTAMPTZ")
+  var deletedAt: Instant? = null
+
+  /**
+   * Soft Delete 실행
+   */
+  fun delete(now: Instant) {
+    this.isDeleted = true
+    deletedAt = now
+  }
+}
