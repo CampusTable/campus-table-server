dd90e6d996e41c467307923e51d9aab88cf4cd07
Chuseok22 <bjh59629@naver.com>
2026-01-12 09:42:54 +0900

    Spring_Security_기본_설정 : feat : MemberRepository.kt pk 기반 조회 로직 추가 https://github.com/CampusTable/campus-table-server/issues/3

--- CT-member/src/main/kotlin/com/chuseok22/ctmember/infrastructure/repository/MemberRepository.kt
+++ CT-member/src/main/kotlin/com/chuseok22/ctmember/infrastructure/repository/MemberRepository.kt
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository
 import java.util.*
 
 interface MemberRepository : JpaRepository<Member, UUID> {
+
+  fun findByIdAndDeletedFalse(memberId: UUID): Member?
   fun findByStudentNameAndDeletedFalse(studentName: String): Member?
 }
