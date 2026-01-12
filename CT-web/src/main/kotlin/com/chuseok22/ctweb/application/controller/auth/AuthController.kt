e21b082e0f0cddca51dbaadb0dd5b470cf177619
Chuseok22 <bjh59629@naver.com>
2026-01-12 15:52:37 +0900

    로그인_및_기타_인증_관련_로직_개발 : feat : Reissue & 로그아웃 컨트롤러 추가 https://github.com/CampusTable/campus-table-server/issues/5

--- CT-web/src/main/kotlin/com/chuseok22/ctweb/application/controller/auth/AuthController.kt
+++ CT-web/src/main/kotlin/com/chuseok22/ctweb/application/controller/auth/AuthController.kt
@@ -1,10 +1,13 @@
 package com.chuseok22.ctweb.application.controller.auth
 
 import com.chuseok22.ctauth.application.dto.request.LoginRequest
+import com.chuseok22.ctauth.application.dto.request.ReissueRequest
 import com.chuseok22.ctauth.application.dto.response.LoginResponse
+import com.chuseok22.ctauth.application.dto.response.ReissueResponse
 import com.chuseok22.ctauth.application.service.AuthService
 import com.chuseok22.logging.annotation.LogMonitoring
 import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -21,5 +24,17 @@ class AuthController(
   @PostMapping("/login")
   fun login(request: LoginRequest): ResponseEntity<LoginResponse> {
     return ResponseEntity.ok(authService.login(request))
+  }
+
+  @LogMonitoring
+  @PostMapping("/reissue")
+  fun reissue(request: ReissueRequest): ResponseEntity<ReissueResponse> {
+    return ResponseEntity.ok(authService.reissue(request))
+  }
+
+  @LogMonitoring
+  @PostMapping("/logout")
+  fun logout(): ResponseEntity<Unit> {
+    return ResponseEntity.status(HttpStatus.NO_CONTENT).build()
   }
 }
